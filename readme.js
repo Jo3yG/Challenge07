2899920123756cb9b44bc4ec5fc0641fae0011c5
Jo3yG <nonuser31@yahoo.com>
2023-10-21 02:42:37 -0500

    added a readMe page that is template

---
+++ readme.js
@@ -0,0 +1,29 @@
+const readMe = data => {
+    
+## Description 
+${data.description}
+
+## Table of Contents 
+*[Installation](#installation)
+*[Usage](#usage)
+*[License](#license)
+*[Contribution](#contribution)
+*[Test](#test)
+
+## Installation
+${data.install}
+
+## Usage
+${data.usage}
+
+## License
+${data.license}
+
+## Contribution
+${contribution}
+
+## Test
+${data.test}
+}
+
+module.exports = readMe;
