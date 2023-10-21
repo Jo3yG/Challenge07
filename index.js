558b8b9f5fc2ee8b5be4840ecef260f8e68b8ce5
Jo3yG <nonuser31@yahoo.com>
2023-10-21 02:26:14 -0500

    added email and github questions

--- index.js
+++ index.js
@@ -5,41 +5,52 @@ const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [
     {
-      type: 'input',
-      message: 'What is the title of your project?',
-      name: 'title',
+     type: 'input',
+     message: 'What is the title of your project?',
+     name: 'title',
     },
     {
-      type: 'input',
-      message: 'Write a description for your Project',
-      name:'description',
+     type: 'input',
+     message: 'Write a description for your Project',
+     name:'description',
     },
     {
-      type: 'input',
-      message: 'Input the installation instructions if any',
-      name:'installation',
+     type: 'input',
+     message: 'Input the installation instructions if any',
+     name:'installation',
     },
     {
-        type: 'input',
-        message: 'Input instructions and examples of your project',
-        name:'installation',
-      },
-      {
-        type: 'input',
-        message: 'Input how other developers can contribute to your Project',
-        name:'contribute',
-      },
-      {
-        type: 'input',
-        message: 'Input any tests written for the application of your Project and possible examples on how to run them.',
-        name:'contribute',
-      },
-      {
-        type: 'list',
-        message: "Would you like a license for your Project?",
-        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
-        name: 'license'
+     type: 'input',
+     message: 'Input instructions and examples of your project',
+     name:'installation',
+    },
+    {
+     type: 'input',
+     message: 'Input how other developers can contribute to your Project',
+     name:'contribute',
+    },
+    {
+     type: 'input',
+     message: 'Input any tests written for the application of your Project and possible examples on how to run them.',
+     name:'contribute',
+    },
+    {
+     type: 'list',
+     message: "Would you like a license for your Project?",
+     choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+     name: 'license'
     }
+    {
+     type: 'input',
+     message: 'What is your email address?',
+     name:'email',
+    },
+    {
+     type: 'input',
+     message: 'What is your GitHub Profile?',
+     name:'github',
+    },
+
   ];
 
 
