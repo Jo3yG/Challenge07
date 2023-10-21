0dc6372d70a50398330d9ca723af6fdd15a15f4c
Jo3yG <nonuser31@yahoo.com>
2023-10-20 23:41:14 -0500

    added questions, need to fetch github api

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: 'input',
+      message: 'What is the title of your project?',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'Write a description for your Project',
+      name:'description',
+    },
+    {
+      type: 'input',
+      message: 'Input the installation instructions if any',
+      name:'installation',
+    },
+    {
+        type: 'input',
+        message: 'Input instructions and examples of your project',
+        name:'installation',
+      },
+      {
+        type: 'input',
+        message: 'Input how other developers can contribute to your Project',
+        name:'contribute',
+      },
+      {
+        type: 'input',
+        message: 'Input any tests written for the application of your Project and possible examples on how to run them.',
+        name:'contribute',
+      },
+      {
+        type: 'list',
+        message: "Would you like a license for your Project?",
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+        name: 'license'
+    }
+  ];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
